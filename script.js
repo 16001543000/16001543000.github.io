d46c0d8116cb0609e5b0d024ab933a08ae4fdbb8
16001543000 <ROSALINDA.GODOY@live.ama.edu.ph>
2024-10-21 19:17:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ document.getElementById("cv-link").addEventListener("click", function(event) {
   event.preventDefault(); // Prevent the default behavior of the link
   window.location.href = "https://www.youtube.com/watch?v=xvFZjo5PgG0"; // Redirect to the Rickroll link
 });
+document.getElementById("ig-link").addEventListener("click", function(event) {
+  event.preventDefault(); // Prevent the default behavior of the link
+  window.location.href = "https://www.threads.net/@msrble_hctb"; // Redirect to instragram profile
