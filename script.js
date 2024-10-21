79c5f84c31e8428695bc7a39ce3b6ec5ed064d87
16001543000 <ROSALINDA.GODOY@live.ama.edu.ph>
2024-10-21 19:17:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,4 @@ document.getElementById("cv-link").addEventListener("click", function(event) {
 document.getElementById("ig-link").addEventListener("click", function(event) {
   event.preventDefault(); // Prevent the default behavior of the link
   window.location.href = "https://www.threads.net/@msrble_hctb"; // Redirect to instragram profile
+});
